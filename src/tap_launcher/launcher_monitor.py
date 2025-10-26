b85217c6b66a1164e9fc56f58b32f72c9a40eab8
aka.namerec <aka.namerec@gmail.com>
2025-10-27 02:07:23 +0300

    debug: disable counter check in release wrapper

--- src/tap_launcher/launcher_monitor.py
+++ src/tap_launcher/launcher_monitor.py
@@ -111,14 +111,15 @@ class LauncherMonitor:
             self.logger.debug(f'[RELEASE] on_release_wrapper called for {key}')
             event_key = (key, False)  # (key, is_press)
             
-            # Check if this is our own emulated event
-            if event_key in self._emulated_events and self._emulated_events[event_key] > 0:
-                self._emulated_events[event_key] -= 1
-                if self._emulated_events[event_key] == 0:
-                    del self._emulated_events[event_key]
-                # Skip - this is our own emulation
-                self.logger.debug(f'Skipping emulated release: {key}')
-                return
+            # TODO: Check if this is our own emulated event
+            # Disabled to debug
+            # if event_key in self._emulated_events and self._emulated_events[event_key] > 0:
+            #     self._emulated_events[event_key] -= 1
+            #     if self._emulated_events[event_key] == 0:
+            #         del self._emulated_events[event_key]
+            #     # Skip - this is our own emulation
+            #     self.logger.debug(f'Skipping emulated release: {key}')
+            #     return
             
             # Check if we emitted the corresponding press
             # If we didn't emit press, don't emit release either (prevent mismatch)
